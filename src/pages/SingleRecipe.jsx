c24d4b72edec5e60f7a710f966524eb3ed3dc6da
Harold Manuel <aquinoharold460@gmail.com>
2023-04-08 23:40:15 -0400

    Setup the media queries

--- src/pages/SingleRecipe.jsx
+++ src/pages/SingleRecipe.jsx
@@ -18,7 +18,7 @@ const SingleRecipe = () => {
           strMeal: name,
           strCategory: category,
           strArea: area,
-          strIntructions: instructions,
+          strInstructions: instructions,
           strMealThumb: img,
           strYoutube: video,
           strIngredient1,
@@ -75,31 +75,61 @@ const SingleRecipe = () => {
   } = singleRecipe;
   return (
     <section className="pt-10 max-w-[1200px] mx-auto">
-      <div className="grid md:grid-cols-2">
+      <div className="grid lg:grid-cols-2  grid-cols-1 items-center px-6">
         <div>
           <img
             src={img}
-            className="max-w-[500px] rounded max-h-[100%]"
+            className="md:max-w-[500px] max-w-[400px] rounded"
             alt=""
           />
         </div>
-        <header>
-          <div className="grid grid-cols-3 gap-3 bg-[#FFFBF5]">
-            <div className=" rounded-sm">
-              <h2 className="">Name recipe</h2>
-              {name}
+        <header className="my-4">
+          <div className="grid md:grid-cols-1 gap-4">
+            <div className="flex items-center gap-3">
+              <span className="bg-[#F7941D] text-white px-3 py-1 rounded-sm">
+                Name:
+              </span>
+              <p className="font-bold">{name}</p>
             </div>
-            <div className="rounded-sm">
-              <h2 className="">Category</h2>
-              {category}
+            <div className="flex items-center gap-3">
+              <span className="bg-[#F7941D] text-white px-3 py-1 rounded-sm">
+                category:
+              </span>
+              <p className="font-bold">{category}</p>
             </div>
-            <div className="rounded-sm">
-              <h2 className="">Area</h2>
-              {area}
+            <div className="flex items-center gap-3">
+              <span className="bg-[#F7941D] text-white px-3 py-1 rounded-sm">
+                Area:
+              </span>
+              <p className="font-bold">{area}</p>
+            </div>
+            <div className="flex items-center gap-3">
+              <span className="bg-[#F7941D] text-white px-3 py-1 rounded-sm">
+                Video:
+              </span>
+              <p className="font-bold">
+                <a href={video} className="underline">
+                  Click to see on youtube
+                </a>
+              </p>
+            </div>
+            <div className="flex items-center gap-3">
+              <span className="bg-[#F7941D] text-white px-3 py-1 rounded-sm">
+                Ingredients:
+              </span>
+              <p className="font-bold">{ingredients + " , "}</p>
             </div>
           </div>
         </header>
       </div>
+      <footer className="grid items-center justify-center grid-cols-1 px-6">
+        <span className="bg-[#F7941D] md:max-w-[300px] mt-4 text-white px-3 py-1 rounded-sm mb-3">
+          Instructions
+        </span>
+        <p className="max-w-[540px] md:max-w-[100%] mx-auto text-justify">
+          {instructions}
+        </p>
+      </footer>
     </section>
   );
 };
